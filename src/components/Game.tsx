5d9c6efc8e44f3e3c6944d022597b99309fdf553
manhph129 <manhph09@gmail.com>
2022-11-24 22:59:36 +0700

    update gif

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -27,11 +27,12 @@ function Game({ gotoMenu, gotoGame }: any) {
     return (
         <div className="row-bg">
 
-            <img alt="background" src="./images/background.png" />
-
+            <img src="./images/background.png" />
+            
             <div className="content-before" >
                 {step == 'START' && <div className="game-cracked-group">
-                    <img className="handle" draggable="false" id="cracker" alt="cracked-top" src="./images/game/cracked-top.png" />
+                    <img className="handle" draggable="false" id="cracker" src="./images/game/cracked-top.png" />
+                    <img id="mechanism" src="./images/game/mechanism.png" />
                     <Draggable
                         axis="y"
                         handle=".handle"
@@ -40,46 +41,61 @@ function Game({ gotoMenu, gotoGame }: any) {
                         scale={1}
                         onDrag={onDrag}
                     >
-                        <img className="handle" draggable="false" id="cracker" alt="cracked-bot" src="./images/game/cracked-bot.png" />
-
+                        <img className="handle" draggable="false" id="cracker" src="./images/game/cracked-bot.png" />
                     </Draggable>
-                    <img id="mechanism" alt="mechanism" src="./images/game/mechanism.png" />
                 </div>}
                 <div className={step == 'ANIMATION' ? '' : 'hidden'}>
-                    <img id="cracked-click" alt="cracked-click" src="./images/game/cracked-click.png" />
-                    <img id="explosion2" alt="explosion2" src="./images/game/explosion2.png" />
-                    <img id="sun" alt="ray" src="./images/game/ray.png" />
-                    <img id="explosion" alt="explosion" src="./images/game/explosion.png" />
-                    <img id="mechanism" alt="mechanism" src="./images/game/mechanism.png" />
+                    <img id="cracked-click" src="./images/game/cracked-click.png" />
+                    <img id="explosion2" src="./images/game/explosion2.png" />
+                    <img id="sun" src="./images/game/ray.png" />
+                    <img id="explosion" src="./images/game/explosion.png" />
+                    <img id="mechanism" src="./images/game/mechanism.png" />
                 </div>
             </div>
 
             {step == 'WIN' && <div className="content-win win-coctail">
-                <img id="text1" alt="text1" src="./images/win/text1.png" />
+                <img id="cocktail" className="img-gif" src="./images/game/cocktail.gif" />
+                <img id="text1" src="./images/win/text1.png" />
                 <div className="enter-code">
-                    <img id="unique" alt="unique" src="./images/win/unique.png" />
+                    <img id="unique" src="./images/win/unique.png" />
                     <input placeholder="Unique code here" />
                 </div>
-                <img id="text2" alt="text2" src="./images/win/text2.png" />
-                <img id="hendrick" alt="hendrick" src="./images/win/hendrick.png" />
+                <img id="text2" src="./images/win/text2.png" />
+                <img id="hendrick" src="./images/win/hendrick.png" />
                 <div className="icon-win">
-                    <img className="ic1" alt="ic1" src="./images/win/ic1.png" />
-                    <img className="ic2" alt="ic2" src="./images/win/ic2.png" />
-                    <img className="ic3" alt="ic3" src="./images/win/ic3.png" />
-                    <img className="ic4" alt="ic4" src="./images/win/ic4.png" />
-                    <img className="ic5" alt="ic5" src="./images/win/ic5.png" />
+                    <img className="ic1" src="./images/win/ic1.png" />
+                    <img className="ic2" src="./images/win/ic2.png" />
+                    <img className="ic3" src="./images/win/ic3.png" />
+                    <img className="ic4" src="./images/win/ic4.png" />
+                    <img className="ic5" src="./images/win/ic5.png" />
                 </div>
-                <img id="redeemed" alt="redeemed" src="./images/win/redeemed.png" />
             </div>}
+            <div className="content-win win-socks" hidden>
+                <img id="cocktail" className="img-gif" src="./images/game/socks.gif" />
+                <img id="text1" src="./images/win/text1socks.png" />
+                <div className="enter-code">
+                    <img id="unique" src="./images/win/unique.png" />
+                    <input placeholder="Unique code here" />
+                </div>
+                <img id="text2" src="./images/win/text2.png" />
+                <img id="hendrick" src="./images/win/hendrick.png" />
+                <div className="icon-win">
+                    <img className="ic1" src="./images/win/ic1.png" />
+                    <img className="ic2" src="./images/win/ic2.png" />
+                    <img className="ic3" src="./images/win/ic3.png" />
+                    <img className="ic4" src="./images/win/ic4.png" />
+                    <img className="ic5" src="./images/win/ic5.png" />
+                </div>
+            </div>
 
             {step == 'LOSE' && <div className="content-lose">
-                <img id="title" alt="title" src="./images/lose/title.png" />
+                <img id="lose" className="img-gif"  src="./images/lose/lose.gif" />
+                <img id="title" src="./images/lose/title.png" />
                 <p className="follow">Follow us at @handricksginsea<br />for more fetive goodness!</p>
                 <a className="link-right" href="./menu.html">
-                    <img id="right" alt="right" src="./images/lose/right.png" />
+                    <img id="right" src="./images/lose/right.png" />
                 </a>
-
-                <img id="put" alt="put" src="./images/lose/put.png" />
+                <img id="put" src="./images/lose/put.png" />
             </div>
             }
         </div>
