3206b5eb068c592700898a2c81f4cc233acac0cf
manhph129 <manhph09@gmail.com>
2022-11-27 11:14:31 +0700

    update oh-dear page

--- src/components/OhDear.tsx
+++ src/components/OhDear.tsx
@@ -1,20 +1,16 @@
 import React, { useEffect } from 'react';
 function OhDear({ closeIframe }: any) {
     return <div className="row-bg">
-        {/* <img className='is-desktop' src="./images/oh-dear/dear-bg.png" />
-        <img className='is-mobile' src="./images/oh-dear/bg-mob.png" /> */}
-        <img src="./images/oh-dear/oh-dear-bg.jpg" />
+        {/* <img className='is-desktop' src="./images/oh-dear/oh-dear-bg.jpg" /> */}
+        <img src="./images/oh-dear/oh-dear-bg-mob.jpg" />
         <div className="content-dear" >
-            {/* <img id="logo" src="./images/home/logo.png" />
-            <img id="oh-dear" src="./images/oh-dear/oh-dear.png" />
-            <p className="you-must">Unfortunately, you only have one crack<br />at the Hendrick's Festive Cracker. 'Til next year!</p> */}
+            
             <div className="two-btn">
                 {/* <a onClick={() => closeIframe()}><img id="left" src="./images/oh-dear/left.png" /></a>
                 <a onClick={() => closeIframe()}><img id="left" src="./images/oh-dear/right.png" /></a> */}
                 <a onClick={() => closeIframe()}><img id="left" src="./images/oh-dear/left.png" /></a>
                 <a onClick={() => closeIframe()}><img id="left" src="./images/oh-dear/right.png" /></a>
             </div>
-            {/* <img id="bot-flower" src="./images/oh-dear/bot-flower.png" /> */}
             <p><a className='link-ig' href='https://www.instagram.com/hendricksginsea' target="_blank"></a></p>
         </div>
     </div>
