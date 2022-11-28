cd52506aa0be145622f0e21d737a95bbc6626572
manhph129 <manhph09@gmail.com>
2022-11-28 22:33:49 +0700

    update win img bot

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -60,7 +60,7 @@ function Game({ result }: any) {
 
     }
     return (
-        <div className="row-bg">
+        <div className="row-bg row-win">
             <img className={`${step == 'WIN'? 'opacity-0' : ''}`} src="./images/game/bg-unbox.jpg" />
             <div className={`content-before ${step == 'WIN' || step == 'LOSE' ? 'hidden' : ''}`} >
                 {step == 'START' && <div className="game-cracked-group">
@@ -95,7 +95,7 @@ function Game({ result }: any) {
                 <img id="redeemed" onClick={() => closePopup()} alt="redeemed" src="./images/win/redeemed.png" />
             </div>}
             {step == 'WIN' && <div className="content-win win-coctail">
-                <img className='bg-only-win' src="./images/background3.png" />
+                <img className='bg-only-win is-desktop' src="./images/background3.png" />
                 <img id="cocktail" className="img-gif" src="./images/game/boom.gif" />
                 {result.couponType == 'sock' ? <img id="text1" src="./images/win/text1socks.png" /> : <img id="text1" src="./images/win/text1.png" />}
 
@@ -124,9 +124,14 @@ function Game({ result }: any) {
                     <img className="ic4" src="./images/win/ic4.png" />
                     <img className="ic5" src="./images/win/ic5.png" />
                 </div>
+                <div className='win-bot is-mobile'>
+                    <img className='win-bot-img' src="./images/win/win-top.jpg" />
+                    <img className='win-bot-img' src="./images/win/win-bot.jpg" />
+                </div>
+                
             </div>}
             {/* <div className="content-win win-socks" hidden>
-                <img className='bg-only-win' src="./images/background3.png" />
+                <img className='bg-only-win is-desktop' src="./images/background3.png" />
                 <img id="cocktail" className="img-gif" src="./images/game/boom.gif" />
                 <img id="text1" src="./images/win/text1socks.png" />
                 <div className="enter-code">
@@ -141,6 +146,10 @@ function Game({ result }: any) {
                     <img className="ic3" src="./images/win/ic3.png" />
                     <img className="ic4" src="./images/win/ic4.png" />
                     <img className="ic5" src="./images/win/ic5.png" />
+                </div>
+                <div className='win-bot is-mobile'>
+                    <img className='win-bot-img' src="./images/win/win-top.jpg" />
+                    <img className='win-bot-img' src="./images/win/win-bot.jpg" />
                 </div>
             </div> */}
 
